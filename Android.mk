18b588c6327088cc13c628e78ae2853bfb55e7e6
Ethan Chen <intervigil@gmail.com>
2025-07-29 10:57:41 +0800

    OP46B1: Create libGLESv2_adreno.so symlink
    * libGLESv2_adreno.so is needed on camera blobs
    
    Change-Id: Iced972a02073e0c327e02a359870aeec299af0bd
    Signed-off-by: Ethan Chen <intervigil@gmail.com>

--- Android.mk
+++ Android.mk
@@ -8,4 +8,12 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),OP46B1)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+EGL_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libGLESv2_adreno.so
+$(EGL_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf egl/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(EGL_SYMLINK)
+
 endif
