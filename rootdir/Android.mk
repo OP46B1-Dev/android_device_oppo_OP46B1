c6208cf48812a6894dc9badcb36c4bd1d3582953
WenHao2130 <wenhao2130@outlook.com>
2025-07-29 15:38:55 +0800

    ff3
    Change-Id: I0b8e32806d0e312759a12aaa4f1e150cfe99e5a3
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -12,7 +12,7 @@ LOCAL_MODULE       := init.class_main.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := bin/init.class_main.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -68,7 +68,7 @@ LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)/etc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -116,7 +116,7 @@ LOCAL_MODULE       := init.recovery.qcom.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_COPY_OUT_RECOVERY)/root
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 # fstab
