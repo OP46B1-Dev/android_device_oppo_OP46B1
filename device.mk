d9ecad01e4f5d9fa69856cde4704fc243f06394e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2025-09-06 21:12:26 +0800

    OP46B1: Use QTI USB init configs from USB HAL repo
    Change-Id: I966d251a2756c9d9e91f621cbc5872448d022de4
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- device.mk
+++ device.mk
@@ -325,7 +325,8 @@ PRODUCT_COPY_FILES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    vendor/qcom/opensource/usb/etc
 
 # Telephony
 PRODUCT_PACKAGES += \
