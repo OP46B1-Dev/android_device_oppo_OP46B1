69792b1e0e58f8e0ae2e8949cf2f47da3f5292a4
LuK1337 <priv.luk@gmail.com>
2025-09-06 20:01:41 +0800

    OP46B1: camera_helper: Import camera_helper from guacamole
    Change-Id: I3bf3675d17a4bbc5d1f2169c9362fea40dc5dbfb
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- device.mk
+++ device.mk
@@ -83,6 +83,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+# Camera Helper
+PRODUCT_PACKAGES += \
+    OnePlusCameraHelper
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl-qti-display \
