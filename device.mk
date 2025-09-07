be19feb9f719d9021d605b5bf75b9a41d80c67d8
WenHao2130 <wenhao2130@outlook.com>
2025-09-07 14:47:24 +0800

    OP46B1: configs: Move sensors config to configs directory
    Change-Id: I9d716b4f851548722ddad4c43267e76abff75211
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- device.mk
+++ device.mk
@@ -321,7 +321,7 @@ PRODUCT_PACKAGES += \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
