5659b743fbce1be2b374fec97f781daf84d480cd
WenHao2130 <wenhao2130@outlook.com>
2025-12-16 00:13:45 +0800

    OP46B1: releasetools: Drop unused re and hashlib
    Change-Id: I4c9ab5e2b6547339a8a7631c4c996f27044a7415
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -14,9 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import hashlib
 import common
-import re
 
 def LoadFilesMap(zip):
   try:
