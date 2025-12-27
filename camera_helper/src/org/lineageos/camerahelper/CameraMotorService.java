a5f18b4b0255a2f139b4789c7b96db0c5a9f2227
LuK1337 <priv.luk@gmail.com>
2025-12-28 01:33:02 +0800

    OP46B1: camera_helper: Call correct super method
    Change-Id: I394647c0aa029d968d92520e9838e5892326bf7f
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- camera_helper/src/org/lineageos/camerahelper/CameraMotorService.java
+++ camera_helper/src/org/lineageos/camerahelper/CameraMotorService.java
@@ -76,7 +76,7 @@ public class CameraMotorService extends Service implements Handler.Callback {
 
                 @Override
                 public void onCameraUnavailable(@NonNull String cameraId) {
-                    super.onCameraAvailable(cameraId);
+                    super.onCameraUnavailable(cameraId);
 
                     if (cameraId.equals(FRONT_CAMERA_ID)) {
                         mIsFontCameraOn = true;
