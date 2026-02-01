82758f7c6a5e2f737c9a3a79e32e9ef5ed37fb4d
Bruno Martins <bgcngm@gmail.com>
2026-02-01 22:48:18 +0800

    OP46B1: Migrate to restructured sepolicy_vndr
    The new folder structure now looks like this:
    
      device/qcom/sepolicy_vndr
      ├─ legacy-um (meant for sdm845 until sm8350)
      ├─ sm8450
      └─ sm8550
    
    Change-Id: I4ed1a06af779909d64172da294c4a99a3ae9ba0d
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,7 +137,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
 VENDOR_SECURITY_PATCH := 2022-04-05
 
 # Sepolicy
-include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 # Treble
 BOARD_VNDK_VERSION := current
