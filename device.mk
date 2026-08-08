a3b4dc82ec19fb58d00adec2b6acfa18ebbce5cd
Michael Bestas <mkbestas@lineageos.org>
2026-08-08 15:59:55 +0800

    OP46B1: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- device.mk
+++ device.mk
@@ -260,6 +260,12 @@ PRODUCT_PACKAGES += \
     TelephonyResOverlayOP46B1 \
     WifiOverlayOP46B1
 
+# Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
