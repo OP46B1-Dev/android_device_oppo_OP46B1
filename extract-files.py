5a26b220025e4cd016315310f9ddcb4c05664dea
WenHao2130 <wenhao2130@outlook.com>
2026-08-08 15:18:45 +0800

    OP46B1: Switch to py extract utils
    Change-Id: I19b5c4179c1103582d6a1468334ff8ac484e294b
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

---
+++ extract-files.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.file import File
+from extract_utils.fixups_blob import (
+    BlobFixupCtx,
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/oppo/OP46B1',
+    'hardware/qcom-caf/sdm845',
+    'hardware/qcom-caf/wlan',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'odm/lib64/oplus.sensors.ssc.so': blob_fixup()
+        .binary_regex_replace(
+            b'android.sensor.tp_proximity',
+            b'android.sensor.proximity\x00\x00\x00'
+        )
+        .sig_replace(
+            '00 40 26 FA 01 00 00 A0 40', # mov w1, #0x1fa2640
+            '00 08 00 00 00 00 00 A0 40'  # mov w1, #8
+        ),
+    (
+        'odm/bin/hw/vendor.oplus.hardware.biometrics.fingerprint@2.1-service',
+        'odm/lib64/libgf_hal_G2.so',
+        'odm/lib64/libgf_hal_G3.so',
+        'odm/lib64/libgf_hal_G5.so',
+        'odm/lib64/libgf_hal_G6.so'
+    ): blob_fixup()
+        .binary_regex_replace(
+            b'odm/vendor/firmware',
+            b'odm/firmware\x00\x00\x00\x00\x00\x00\x00'
+        ),
+    'system_ext/lib64/lib-imsvideocodec.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
+    'vendor/lib64/hw/camera.qcom.so': blob_fixup()
+        .add_needed('liboplus_camera_shim.so'),
+    'vendor/lib64/sensors.ssc.so': blob_fixup()
+        .binary_regex_replace(
+            b'qti.sensor.wise_light',
+            b'android.sensor.light\x00'
+        )
+        .sig_replace(
+            'F9 69 CA 84 52 49 3F A0 72', # mov w9, #0x1fa2653
+            'F9 A9 00 80 52 09 00 A0 72'  # mov w9, #5
+        ),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'OP46B1',
+    'oppo',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
